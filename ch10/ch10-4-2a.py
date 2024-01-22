cd77ab9a4d13d689477d822b110d688a3fb37df8
hyseanlin <hylin827@gmail.com>
2024-01-22 20:17:16 +0800

    hotfix: 1. the parameter ci is deprecated, so replace it with errobar; 2. the parameter estimator is no loger used. 3. change the way of drawing the last plot as 'scatter'

--- ch10/ch10-4-2a.py
+++ ch10/ch10-4-2a.py
@@ -6,10 +6,9 @@ df = sns.load_dataset("fmri")
 sns.set()
 sns.relplot(x="timepoint", y="signal", data=df, kind="line")
 
-sns.relplot(x="timepoint", y="signal", ci=None, data=df, kind="line")
-sns.relplot(x="timepoint", y="signal", ci="sd", data=df, kind="line")
+sns.relplot(x="timepoint", y="signal", errorbar=None, data=df, kind="line")
+sns.relplot(x="timepoint", y="signal", errorbar="sd", data=df, kind="line")
 
-sns.relplot(x="timepoint", y="signal", 
-            estimator=None, data=df, kind="line")
+sns.relplot(x="timepoint", y="signal", data=df, kind='scatter')
 
 plt.show()
